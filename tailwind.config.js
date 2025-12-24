4049c8e986219f3b8327e66f4e42365b7f3a0a19
zyd16888 <26684563+zyd16888@users.noreply.github.com>
2025-12-24 21:48:15 +0800

    UI 从 Layui 换成 DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./internal/server/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["corporate", "dark"],
+  },
+};
+
